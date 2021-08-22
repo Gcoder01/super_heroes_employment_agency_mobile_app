5086e60e00e31f5fe3e10ea25a80ea572ffdaf45
haithamswe <haitham.1419@outlook.sa>
2021-08-22 23:00:39 +0300

    -

--- lib/logic/db_endpoints.dart
+++ lib/logic/db_endpoints.dart
@@ -1,4 +1,6 @@
 //singlton
+import 'dart:convert';
+
 import 'package:flutter/services.dart';
 
 class Database {
@@ -8,14 +10,21 @@ class Database {
   }
 
   readDB() async {
-    var jsonText = await rootBundle.loadString('assets/data.json');
+    //const JsonCodec json = JsonCodec();
+
+    /*var*/ jsonText = await rootBundle.loadString('assets/data.json');
+    jsonText = json.decode(jsonText);
     print(jsonText);
     print(111111111111);
   }
 }
+  var jsonText;
 
 main() {
   var x = Database.instanse;
-      print(rootBundle.loadString('assets\data.json'));
+      print(9);
+      print(x.readDB());
+      print(jsonText);
+      print(00);
 
 }
